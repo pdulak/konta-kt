f8c63fb97503ac15b3bc403524b55bcfffb378dd
Pawel Dulak <pawel.dulak@gmail.com>
2018-12-22 23:17:38 +0100

    first model added

--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Bank(models.Model):
+    bank_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.bank_name
+
+class Account(models.Model):
+    bank = models.ForeignKey(Bank, on_delete=models.PROTECT)
+    account_name = models.CharField(max_length=200)
+    account_number = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.account_name
+
