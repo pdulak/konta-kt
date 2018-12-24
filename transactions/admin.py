3782f09d9846f1c2aa52bf9786ceea9d7c6551c5
Pawel Dulak <pawel.dulak@gmail.com>
2018-12-24 13:40:21 +0100

    transactions added, accounts adjusted

--- transactions/admin.py
+++ transactions/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import CategoryGroup, Category, TransactionType, Transaction
+
+admin.site.register(CategoryGroup)
+admin.site.register(Category)
+admin.site.register(TransactionType)
+admin.site.register(Transaction)
--- transactions/models.py
+++ transactions/models.py
@@ -1,3 +1,40 @@
 from django.db import models
 
 # Create your models here.
+
+class CategoryGroup(models.Model):
+    group_name = models.CharField(max_length=100)
+    income = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.group_name
+
+class Category(models.Model):
+    category_group = models.ForeignKey(CategoryGroup, on_delete=models.PROTECT, null=True, blank=True)
+    category_name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.category_name
+
+    class Meta:
+        verbose_name_plural = "categories"
+
+class TransactionType(models.Model):
+    type_name = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.type_name
+
+class Transaction(models.Model):
+    account = models.ForeignKey('accounts.Account', on_delete=models.PROTECT)
+    type = models.ForeignKey(TransactionType, on_delete=models.PROTECT)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    uuid_text = models.CharField(max_length=200)
+    transaction_date = models.DateField()
+    transaction_added = models.DateField()
+    amount = models.DecimalField(max_digits=12, decimal_places=2)
+    transaction_description = models.CharField(max_length=2000)
+    transaction_imported_description = models.CharField(max_length=2000)
+    party_name = models.CharField(max_length=500)
+    party_IBAN = models.CharField(max_length=200)
+    irrelevant = models.BooleanField(default=False)
