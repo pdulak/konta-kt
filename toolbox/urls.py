ca484c51f3eebec8a5fd9642c343b180eb197f4e
Pawel Dulak <pawel.dulak@gmail.com>
2018-12-26 13:12:31 +0100

    reading Kontomierz CSV - implementation in progress

--- toolbox/urls.py
+++ toolbox/urls.py
@@ -6,5 +6,6 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('cleanup_database/', views.cleanup_database, name='cleanup_database'),
     path('initialize_database/', views.initialize_database, name='initialize_database'),
+    path('load_kontomierz/', views.load_kontomierz, name='load_kontomierz'),
     path('review_database/', views.review_database, name='review_database'),
 ]
--- toolbox/views.py
+++ toolbox/views.py
@@ -1,4 +1,10 @@
+import os
+import re
+import glob
+import pandas as pd
+
 from django.shortcuts import render
+from django.conf import settings
 from django.http import HttpResponse
 
 from accounts.models import Account, Bank, Currency
@@ -35,3 +41,37 @@ def review_database(request):
         'all_transactions_list': all_transactions_list,
     }
     return render(request, 'toolbox/review.html', context)
+
+
+def load_kontomierz(request):
+    # load all csv files from temp dir
+    sourceDir = os.path.join(settings.BASE_DIR, 'temp')
+    field_names = ['Bank','Nazwa konta','UID transakcji','Data transakcji','Data ksiêgowania','Kwota','Saldo','Opis','Tytu³','Rodzaj','Strona','IBAN strony','Kategoria','Tagi','Nieistotna']
+    df = pd.DataFrame()
+
+    first_one = re.compile(r'^"')
+    last_one = re.compile(r'"$')
+
+    this_data = ''
+    list_of_files = glob.glob(os.path.join(sourceDir, '*.csv'))
+    for this_file in list_of_files:
+        print('adding: ', this_file)
+        with open(this_file, encoding='cp1250') as f:
+            for line in f:
+                temp_data = last_one.sub("|", line)
+                temp_data = first_one.sub("|", temp_data)
+                temp_data = temp_data.replace('";"', "|;|").replace('";', "|;").replace(';"', ";|")
+                this_data += temp_data
+
+        df_temp = pd.read_csv(pd.compat.StringIO(this_data), sep=';', comment='#', engine='python', names=field_names, skiprows=10,
+                              quotechar="|", encoding='cp1250')
+        df = df.append(df_temp, ignore_index=True)
+        print('added: ',  df_temp.shape)
+
+    print("Whole dataframe shape: ", df.shape)
+    print(df.info())
+
+    # extract unique values to insert into the database
+    
+
+    return HttpResponse("Loading CSV;")
