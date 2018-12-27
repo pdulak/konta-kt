7b75b4628609e643406d8c3f4d3e50ab4e1414f2
Pawel Dulak <pawel.dulak@gmail.com>
2018-12-27 20:59:58 +0100

    advanced montly review query

--- transactions/urls.py
+++ transactions/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('monthly_review/', views.monthly_review, name='monthly_review'),
     path('', views.index, name='index'),
 ]
--- transactions/views.py
+++ transactions/views.py
@@ -1,6 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.db.models import Count, Sum, F, Q
+from django.db.models.functions import TruncMonth, TruncYear
+
+from .models import Transaction
 
 
 def index(request):
     return HttpResponse("Hello, world. You're at the transactions index.")
+
+
+def monthly_review(request):
+    months_list = Transaction.objects.filter(irrelevant=False)\
+        .values(month=TruncMonth('date'))\
+        .annotate(
+            total=Sum('amount'),
+            down=Sum('amount', filter=Q(amount__lt=0)),
+            up=Sum('amount', filter=Q(amount__gt=0)),
+        )\
+        .order_by('month')
+
+    context = {
+        'months_list': months_list,
+    }
+    return render(request, 'transactions/monthly_review.html', context)
