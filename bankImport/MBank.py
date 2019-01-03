f9992dfde6ad2819ff4320bb0f6e2cadbaf8b00d
Pawel Dulak <pawel.dulak@gmail.com>
2019-01-03 22:39:37 +0100

    mBank import finished

--- bankImport/MBank.py
+++ bankImport/MBank.py
@@ -1,7 +1,7 @@
+import os
 import unittest
+import getpass
 from bankImport import SeleniumDrivers
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.support.ui import Select
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions as EC
@@ -10,8 +10,11 @@ class MBank(unittest.TestCase):
 
     @classmethod
     def setUpClass(self):
-        SeleniumDrivers.user_pass = input("Enter pass: ")
-        self.driver = SeleniumDrivers.load_driver()
+        SeleniumDrivers.user_pass = getpass.getpass("Enter pass: ")
+        base_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+        dest_dir = os.path.join(base_dir, 'temp')
+        self.driver = SeleniumDrivers.load_driver(dest_dir)
         driver = self.driver
         driver.get("https://online.mbank.pl/pl/Login/")
 
@@ -38,10 +41,43 @@ class MBank(unittest.TestCase):
             EC.presence_of_element_located((By.XPATH, "//span[contains(text(), 'Zmień widok')]"))
         )
         element.click()
+
+        # switch to iFrame
+        driver.get("https://online.mbank.pl/csite/account_oper_list.aspx")
         # wait for select
         accountsCombo = WebDriverWait(driver, 5).until(
-            EC.presence_of_element_located((By.ID, "rangepanel"))
+            EC.presence_of_element_located((By.ID, "MenuAccountsCombo"))
         )
+
+        # select proper options on the form
+        driver.find_element_by_id("lastdays_radio").click()
+        days_field = driver.find_element_by_id("lastdays_days")
+        days_field.clear()
+        days_field.send_keys("4")
+        period_select = driver.find_element_by_id("lastdays_period")
+        period_select.click()
+        period_options = [x for x in period_select.find_elements_by_tag_name("option")]
+        period_options[1].click()
+
+        driver.find_element_by_id("export_oper_history_check").click()
+        format_select = driver.find_element_by_id("export_oper_history_format")
+        format_select.click()
+        format_options = [x for x in format_select.find_elements_by_tag_name("option")]
+        format_options[2].click()
+
+        # loop by accounts, get CSV
+        accountsCombo = WebDriverWait(driver, 5).until(
+            EC.presence_of_element_located((By.ID, "MenuAccountsCombo"))
+        )
+        accounts = [x for x in accountsCombo.find_elements_by_tag_name("option")]
+        number_of_accounts = len(accounts)
+
+        for i in range(0, number_of_accounts):
+            accountsCombo = driver.find_element_by_id("MenuAccountsCombo")
+            accountsCombo.click()
+            accounts = [x for x in accountsCombo.find_elements_by_tag_name("option")]
+            accounts[i].click()
+            driver.find_element_by_id("Submit").click()
 
     def tearDown(self):
         # nothing to do
--- bankImport/SeleniumDrivers.py
+++ bankImport/SeleniumDrivers.py
@@ -6,20 +6,25 @@ current_driver = "Chrome"
 user_name = "mbank_user_name_here"
 user_pass = ""
 
-def load_driver():
+def load_driver(download_dir=None):
     if current_driver == "Chrome":
-        return load_chrome()
+        return load_chrome(download_dir)
     else:
-        return load_firefox()
+        return load_firefox(download_dir)
 
-def load_firefox():
+def load_firefox(download_dir=None):
     driver = webdriver.Firefox()
     driver.maximize_window()
     return driver
 
 
-def load_chrome():
+def load_chrome(download_dir=None):
     options = Options()
     options.add_argument("--start-maximized")
+
+    if download_dir is not None:
+        prefs = {"download.default_directory": download_dir}
+        options.add_experimental_option("prefs", prefs)
+
     driver = webdriver.Chrome(chrome_options=options)
     return driver
