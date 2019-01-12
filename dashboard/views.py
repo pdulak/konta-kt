3b6a8373caa243238132857bffe3026a26485343
Pawel Dulak <pawel.dulak@gmail.com>
2019-01-12 12:24:16 +0100

    ajax based transactions list added

--- dashboard/views.py
+++ dashboard/views.py
@@ -9,7 +9,6 @@ def index(request):
     context = {
         'accounts_list': get_account_balance(),
         'months_list': get_monthly_review()[:5],
-        'transactions_list': get_transactions_review()[:100],
     }
     return render(request, 'dashboard/dashboard.html', context)
 
