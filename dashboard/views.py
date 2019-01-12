d4dc892e3082d31f3133689d4c288be620d48019
Pawel Dulak <pawel.dulak@gmail.com>
2019-01-12 12:23:54 +0100

    ajax based transactions list added

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.http import JsonResponse
 
 from transactions.views import get_account_balance, get_monthly_review, get_transactions_review
 
@@ -11,3 +12,10 @@ def index(request):
         'transactions_list': get_transactions_review()[:100],
     }
     return render(request, 'dashboard/dashboard.html', context)
+
+
+def j_transactions(request):
+    data = {
+        'transactions_list': list(get_transactions_review()[:100]),
+    }
+    return JsonResponse(data)
