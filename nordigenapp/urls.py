155ee9d55730cc6d954d1a6c6d5f191acf1fa79b
Pawel Dulak <pawel.dulak@gmail.com>
2023-02-26 08:37:35 +0100

    feat: nordigen api discovery in progress

--- nordigenapp/urls.py
+++ nordigenapp/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    path('', views.index, name='nordigen_index'),
+    path('bank_list/', views.bank_list, name='nordigen_bank_list'),
+    path('log_response/', views.log_response, name='nordigen_log_response'),
+    path('test/', views.test, name='nordigen_test'),
 ]
--- nordigenapp/views.py
+++ nordigenapp/views.py
@@ -66,10 +66,58 @@ def get_list_of_banks():
     token_data = client.generate_token()
     return client.institution.get_institutions("PL")
 
+def initialize_bank_connection(institution_id):
+    env = environ.Env()
+    environ.Env.read_env(os.path.join(settings.BASE_DIR, '.env'))
+    #Initialize bank session
+    client = NordigenClient(
+        secret_id=env('NORDIGEN_ID'),
+        secret_key=env('NORDIGEN_SECRET')
+    )
+    token_data = client.generate_token()
+    init = client.initialize_session(
+        # institution id
+        institution_id=institution_id,
+        # redirect url after successful authentication
+        redirect_uri="https://kontakt.dulare.com/nordigen/log_response/",
+        # additional layer of unique ID defined by you
+        reference_id=str(uuid4())
+    )
+    logger.info(init)
+
+def list_requisitions():
+    env = environ.Env()
+    environ.Env.read_env(os.path.join(settings.BASE_DIR, '.env'))
+    #Initialize bank session
+    client = NordigenClient(
+        secret_id=env('NORDIGEN_ID'),
+        secret_key=env('NORDIGEN_SECRET')
+    )
+    token_data = client.generate_token()
+    logger.info(client.requisition.get_requisitions())
+
 @login_required(login_url='/auth/login/')
-def index(request):
+def bank_list(request):
     context = {
         'institutions': get_list_of_banks()
     }
-    logger.info(context)
-    return render(request, 'nordigen/index.html', context)
+    return render(request, 'nordigen/bank_list.html', context)
+
+@login_required(login_url='/auth/login/')
+def index(request):
+    return render(request, 'nordigen/index.html')
+
+@login_required(login_url='/auth/login/')
+def log_response(request):
+    logger.info(request.POST)
+    return render(request, 'nordigen/index.html')
+
+@login_required(login_url='/auth/login/')
+def test(request):
+    # ALIOR_ALBPPLPW
+    # MBANK_RETAIL_BREXPLPW
+    # initialize_bank_connection('ALIOR_ALBPPLPW')
+    list_requisitions()
+    return render(request, 'nordigen/index.html')
+
+
